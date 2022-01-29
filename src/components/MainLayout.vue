b01ebc44aa00ec194bb633f29460868bd096fbda
Andrei <andreivassiljev1@gmail.com>
2022-01-29 16:40:34 +0200

    super basic layout

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <PageHeader />
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-2">
+                <NavigationMenu />
+            </div>
+            <div class="col-10">
+                <Viewer />
+                <ListOfMaterials />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import PageHeader from './PageHeader.vue'
+import NavigationMenu from './NavigationMenu.vue'
+import Viewer from './Viewer.vue'
+import ListOfMaterials from './ListOfMaterials.vue'
+
+export default {
+  name: 'MainLayout',
+  components: {
+    PageHeader,
+    NavigationMenu,
+    Viewer,
+    ListOfMaterials
+  }
+}
+</script>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="p-3 bg-dark text-white">
+        <div>
+            This will be a header component soon.
+        </div>
+    </header>
+</template>
