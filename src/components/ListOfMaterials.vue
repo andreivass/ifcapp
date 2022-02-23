6ad77cd37a50e4fc210da126ba940e1f5281c791
Andrei <andreivassiljev1@gmail.com>
2022-02-23 17:04:50 +0200

    TreeTable test.

--- src/components/ListOfMaterials.vue
+++ src/components/ListOfMaterials.vue
@@ -28,27 +28,82 @@
             </tr>
         </tbody>
     </table>
+    <div v-if="showTreeTable">
+        <VueTreeList :model="treeData"
+        default-tree-node-name="new node"
+        default-leaf-node-name="new leaf"
+        v-bind:default-expanded="false">
+            {{  treeData  }}
+        </VueTreeList>
+    </div>
 </template>
 
 <script>
+import { VueTreeList, Tree } from 'vue-tree-list'
+
 export default {
   name: 'ListOfMaterials',
   props: [
       'ifcTree'
   ],
+  components: {
+      VueTreeList
+  },
+  data() {
+    return {
+        treeData: {},
+        showTreeTable: false
+    }
+  },
   watch: {
       ifcTree: {
           setImmediate: true,
           handler() {
               console.log('Tree value changed');
-              this.prepareIfcTreeData();
+              if (!this.showTreeTable) {
+                this.prepareIfcTreeData();
+              }
+              
           }
       }
   },
   methods: {
       prepareIfcTreeData() 
       {
-        
+        // TODO: parse array data to some structure
+        this.treeData = new Tree([
+          {
+            name: 'Node 1',
+            id: 1,
+            pid: 0,
+            dragDisabled: true,
+            addTreeNodeDisabled: true,
+            addLeafNodeDisabled: true,
+            editNodeDisabled: true,
+            delNodeDisabled: true,
+            children: [
+              {
+                name: 'Node 1-2',
+                id: 2,
+                isLeaf: true,
+                pid: 1
+              }
+            ]
+          },
+          {
+            name: 'Node 2',
+            id: 3,
+            pid: 0,
+            disabled: true
+          },
+          {
+            name: 'Node 3',
+            id: 4,
+            pid: 0
+          }
+        ]);
+
+        this.showTreeTable = true;
       }
   }
 }
