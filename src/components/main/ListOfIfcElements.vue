849795c624c0ef73dd5948f92c865630e3334a94
Andrei Vassiljev <Andreivassiljev1@gmail.com>
2022-09-14 16:42:52 +0300

    Basic modal template.

--- src/components/main/ListOfIfcElements.vue
+++ src/components/main/ListOfIfcElements.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="ifcModelReady == true">
-    <button type="button" @click.prevent="createWp">Uus tööpakett</button>
+    <button type="button" @click.prevent="createWp()">Uus tööpakett</button>
   </div>
     <table class="table table-hover" id="materials-table">
         <thead>
@@ -33,14 +33,36 @@
             </tr>
         </tbody>
     </table>
-    <!-- <div v-if="showTreeTable">
-        <VueTreeList :model="treeData"
-        default-tree-node-name="new node"
-        default-leaf-node-name="new leaf"
-        v-bind:default-expanded="false">
-            {{  treeData  }}
-        </VueTreeList>
-    </div> -->
+
+  <Transition name="modal">
+    <div v-if="show" class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header">default header</slot>
+            <button
+                class="modal-default-button"
+                @click="closeModal"
+              >X</button>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">default body</slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button
+                class="modal-default-button"
+              >Save</button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Transition>
+
 </template>
 
 <script>
@@ -59,7 +81,8 @@ export default {
     return {
         treeData: {},
         showTreeTable: false,
-        selectedItems: new Array
+        selectedItems: new Array,
+        show: false
     }
   },
   watch: {
@@ -81,8 +104,8 @@ export default {
       // });
 
       console.log("Create WP", this.selectedItems);
-
-      this.$emit('ifc-elements-selected', this.selectedItems)
+      this.show = true;
+      //this.$emit('ifc-elements-selected', this.selectedItems)
     },
     elementSelect(element){
       if (this.selectedItems.includes(element)) {
@@ -91,52 +114,79 @@ export default {
       else {
         this.selectedItems.push(element);
       }
+    },
+    closeModal(){
+      this.show = false;
     }
   }
-  // methods: {
-  //     prepareIfcTreeData() 
-  //     {
-  //       // TODO: parse array data to some structure
-  //       this.treeData = new Tree([
-  //         {
-  //           name: 'Node 1',
-  //           id: 1,
-  //           pid: 0,
-  //           dragDisabled: true,
-  //           addTreeNodeDisabled: true,
-  //           addLeafNodeDisabled: true,
-  //           editNodeDisabled: true,
-  //           delNodeDisabled: true,
-  //           children: [
-  //             {
-  //               name: 'Node 1-2',
-  //               id: 2,
-  //               isLeaf: true,
-  //               pid: 1
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           name: 'Node 2',
-  //           id: 3,
-  //           pid: 0,
-  //           disabled: true
-  //         },
-  //         {
-  //           name: 'Node 3',
-  //           id: 4,
-  //           pid: 0
-  //         }
-  //       ]);
-
-  //       this.showTreeTable = true;
-  //     }
-  // }
 }
 </script>
 
 <style>
 #materials-table {
     min-height: 200px;
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
