1d4b51d2b90bbcbb0ecc0b7f61b01b328813e01e
Andrei <andreivassiljev1@gmail.com>
2022-10-07 18:12:51 +0300

    Add prod build variables as comments.

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 export default axios.create({
+  // Use for prod build:
+  // baseURL: "http://madli.me:5208",
   baseURL: "/api",
   headers: {
     "Content-type": "application/json",
