649332cf566336bff7917a97a3f156c5a0b4ca31
Andrei <andreivassiljev1@gmail.com>
2022-12-18 10:07:21 +0200

    Add test IFC file link to main page.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
-    Kliendi teekonna skeem:
-    <img id="flow-chart" src="\ifc-app-flow-chart.png" />
+    <p>Testimise eesmärgil võib alla laadida järgnev test IFC fail: <a href="\KT-CCI_EK_220505.ifc">alla laadimine</a>.</p>
+    <p>Kliendi teekonna skeem:
+    <img id="flow-chart" src="\ifc-app-flow-chart.png" /></p>
 </template>
 
 <script>
--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 export default axios.create({
   // Use for prod build:
-  // baseURL: "http://dreifc.live:5208",
-  baseURL: "/api",
+  baseURL: "http://dreifc.live:5208",
+  // Use for local dev run:
+  // baseURL: "/api",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*"
